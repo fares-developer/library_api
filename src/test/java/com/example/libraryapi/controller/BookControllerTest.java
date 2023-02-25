a52330867f955ee72542d1ec5086f96f1e654204
fares-developer <faresendongpro@gmail.com>
2023-02-25 02:51:09 +0100

    adding unitary test

---
+++ src/test/java/com/example/libraryapi/controller/BookControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.libraryapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getBook() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void testCreate() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}
